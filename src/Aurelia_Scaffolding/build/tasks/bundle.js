c80da17e2f3b436bbaa4740b745d38300102684e
CloudSpire Technology <cloudspire@hotmail.com>
2016-07-27 20:18:10 -0500

    Part 3

---
+++ src/Aurelia_Scaffolding/build/tasks/bundle.js
@@ -0,0 +1,38 @@
+﻿var gulp = require('gulp');
+var bundler = require('aurelia-bundler');
+
+var vendor_config = {
+    force: true,
+    baseURL: './wwwroot',
+    configPath: './wwwroot/config.js',
+    bundles: {
+        "src/vendor-build": {
+            includes: [
+                'aurelia-bootstrapper',
+                'aurelia-dependency-injection',
+                'aurelia-framework',
+                'aurelia-templating-binding',
+                'aurelia-templating-resources',
+                'aurelia-loader-default',
+                'aurelia-fetch-client',
+                'aurelia-router',
+                'aurelia-templating-router',
+                'aurelia-history-browser',
+                'aurelia-logging-console',
+                'aurelia-event-aggregator'
+            ],
+            options: {
+                inject: true,
+                minify: true
+            }
+        }
+    }
+};
+
+gulp.task('bundle-vendor', function () {
+    return bundler.bundle(vendor_config);
+});
+
+gulp.task('unbundle-vendor', function () {
+    return bundler.unbundle(vendor_config);
+});
